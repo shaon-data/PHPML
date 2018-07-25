2f6e43c1da980c913b0e58a752d59ae85f750144
Shaon Majumder <smazoomder@gmail.com>
2018-07-25 08:56:53 -0500

    git

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include __DIR__ . '/php-ml/Dataset/Dataset.php';
 include __DIR__ . '/php-ml/Dataset/ArrayDataset.php';
 include __DIR__ . '/php-ml/Dataset/CsvDataset.php';
+
 include __DIR__ . '/php-ml/Math/Distance.php';
 include __DIR__ . '/php-ml/Math/Distance/Euclidean.php';
 include __DIR__ . '/php-ml/Estimator.php';
@@ -11,7 +12,7 @@ include __DIR__ . '/php-ml/Helper/Predictable.php';
 include __DIR__ . '/php-ml/Classification/KNearestNeighbors.php';
 
 $dataset = new CsvDataset('php-ml/data/iris.csv', 2, true);
-print_r($dataset->getColumnNames());
+print_r($dataset->getTargets());
 
 $samples = [[1, 3], [1, 4], [2, 4], [3, 1], [4, 1], [4, 2]];
 $labels = ['a', 'a', 'a', 'b', 'b', 'b'];
