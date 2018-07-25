901824e6634449f473ebd5caab7e7d137ea093bf
Shaon Majumder <smazoomder@gmail.com>
2018-07-25 09:47:30 -0500

    Rrow

--- Reading_CSV_File.php
+++ Reading_CSV_File.php
@@ -1,17 +1,18 @@
 <?php
 include __DIR__."/init.php";
 $dataset = new CsvDataset('php-ml/data/glass.csv', 10, true);
-$Column_N = ($dataset->getColumnNames());
 
-print("Column Names:<br>");
-$i=0;
-foreach ($Column_N as $arr){
-    print_r(++$i.". ".$arr."<br>");
+function Rcolumn_names($dataset){
+    $rcolumn_names = array();
+    
+    foreach ($dataset->getColumnNames() as $arr){
+        array_push($rcolumn_names, $arr);
+    }
+    return $rcolumn_names;
 }
 
-
 function Rcolumn($dataset){
-    $rcolumn = Array();
+    $rcolumn = array();
     
     foreach($dataset->getSamples() as $row){
         $columncell = $row[0]; //1st column
@@ -21,5 +22,22 @@ function Rcolumn($dataset){
     return $rcolumn;
 }
 
-print_r(Rcolumn($dataset));
+function RshowData($dataset){
+    $i=0;
+    foreach($dataset->getSamples() as $row){
+        print(++$i.". ");
+        print_r($row);
+        print("<br>");
+        //array_push($rcolumn, $columncell);
+    }
+}
+
+function Rrow($dataset, $n){
+    return $dataset->getSamples()[$n];
+}
+
+//print_r(Rrow($dataset, 213));
+//print_r(RshowData($dataset));
+//print_r(Rcolumn_names($dataset));
+//print_r(Rcolumn($dataset));
 ?>
